6b52a0ad845bf2f23552955b2f6cd627de4f3fd4
Phuso <249928583@qq.com>
2019-09-18 20:28:34 +0800

    修改一下测试

--- src/main/java/cn/phuso/flp/controller/PhusoController.java
+++ src/main/java/cn/phuso/flp/controller/PhusoController.java
@@ -14,7 +14,7 @@ public class PhusoController {
     @GetMapping("/hello")
     public String hello(){
 
-        return "I Love Flower";
+        return "I Love Flower111";
     }
 
     @RequestMapping("/test")
